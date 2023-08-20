c7e17e654dde9abc226e6b436087271391bc1370
RuM <135081594+asamiryouta@users.noreply.github.com>
2023-08-21 01:41:51 +0900

    ボス
    ボスが倒されたときシーン移動

--- bottisavivor/Assets/LastBoss.cs
+++ bottisavivor/Assets/LastBoss.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LastBoss : MonoBehaviour
 {
+    int BossHp = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class LastBoss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (BossHp == 0) 
+        {
+            SceneManager.LoadScene("GameClearScene");
+        }
     }
 }
